caacef26c021dad0c39a2eb5bcba416f7a12a3e4
Cholpisit Kiattisewee <cholpk@uw.edu>
2024-03-28 16:52:17 -0700

    update talkmap

---
+++ _talks/130825_Asian-Science-Camp.md
@@ -0,0 +1,13 @@
+---
+title: "National Delegate at the Asian Science Camp 2013"
+collection: talks
+type: "Nobel Laureate Meeting"
+permalink: /talks/130825_Asian-Science-Camp
+venue: "Asian Science Camp 2013"
+date: 2013-08-25
+location: "Tsukuba, Japan"
+---
+
+Ice participated in the Asian Science Camp in Japan as a national delegate from Thailand.
+Here is how excited a freshman undergrad look like seeing a Nobel Laureat (Ada Yonate) for the first time!!!
+<br/><img src='/images/2013_Asian-Science-Camp.jpg'>
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,87 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Tsukuba, Japan",
+    36.0833265,
+    140.077279
+  ],
+  [
+    "Lindau, Germany",
+    47.550753,
+    9.6926624
+  ],
+  [
+    "Jeju, South Korea",
+    33.3939924,
+    126.5626653
+  ],
+  [
+    "Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "United Kingdom",
+    54.7023545,
+    -3.2765753
+  ],
+  [
+    "Suzhou, China",
+    31.3016935,
+    120.5810725
+  ],
+  [
+    "Bangkok, Thailand",
+    13.7524938,
+    100.4935089
+  ],
+  [
+    "Rayong, Thailand",
+    12.7943716,
+    101.3705344
+  ],
+  [
+    "Arlington, Virginia",
+    38.8769326,
+    -77.0893094
+  ],
+  [
+    "Berkeley, California",
     37.8708393,
-    -122.2728638
+    -122.272863
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Portland, Oregon",
+    45.5202471,
+    -122.674194
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Mt. Rainier, Washington",
+    46.8521484,
+    -121.757682
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Seattle, Washington",
+    47.6038321,
+    -122.330062
+  ],
+  [
+    "Bethesda, Maryland",
+    38.98127255,
+    -77.12335871396549
+  ],
+  [
+    "Los Angeles, California",
+    34.0536909,
+    -118.242766
+  ],
+  [
+    "Evanston, Illinois",
+    42.0470043,
+    -87.6846053
+  ],
+  [
+    "Phoenix, Arizona",
+    33.4484367,
+    -112.074141
   ]
 ];
